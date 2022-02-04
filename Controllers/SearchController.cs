39f1fc3e2d1e689d98d78f91171fd660a827cf4d
dev-filbert <filbery.canares@mlhuillier.com>
2022-02-04 18:57:54 +0800

    90% done

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -15,14 +15,45 @@ namespace TechMahindra_Ex.Controllers
         // searchEngine#1 : fbcb2f4d27103455f
 
         // apiKey#2       : AIzaSyAPFJLlisozh1kOdXpPCLcDiH2sS7EWFmI
-        // searchEngine#2 : 129e9516dffb81f9f
+        // searchEngine#2 : 77139038659594bd4
 
-        string apiKey = "AIzaSyD4FFIzIwCYGSk3jx5Fkc5ADiTXFPfbWnE";                  // Got this from https://developers.google.com/custom-search/v1/introduction/?apix=true
-        string searchEngineId = "fbcb2f4d27103455f";                                // Got this from https://cse.google.com/cse/setup/basic?cx=fbcb2f4d27103455f
+        string apiKey = "AIzaSyAPFJLlisozh1kOdXpPCLcDiH2sS7EWFmI";                  // Got this from https://developers.google.com/custom-search/v1/introduction/?apix=true
+        string searchEngineId = "77139038659594bd4";                                // Got this from https://cse.google.com/cse/setup/basic?cx=fbcb2f4d27103455f
 
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult SearchImage()
+        {
+            return View("SearchImage");
+        }
+
+        public ActionResult ShowTopUML(string searchItem)
+        {
+
+            var request = WebRequest.Create("https://www.googleapis.com/customsearch/v1?key=" + apiKey + "&cx=" + searchEngineId + "&q=" + searchItem + "&num=4");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseString = reader.ReadToEnd();
+            dynamic jsonData = JsonConvert.DeserializeObject(responseString);
+
+            var results = new List<SearchResult>();
+            foreach (var item in jsonData.items)
+            {
+                results.Add(new SearchResult
+                {
+                    Title = item.title,
+                    Link = item.link,
+                    displayLink = item.displayLink,
+                    Snippet = item.snippet,
+                });
+            }
+            return Json(new { url = Url.Action("Index", "Home")});
+
+            //return View("ShowResults", results.ToList());
         }
 
         public ActionResult ShowResults()
@@ -62,17 +93,16 @@ namespace TechMahindra_Ex.Controllers
             string responseString = reader.ReadToEnd();
             dynamic jsonData = JsonConvert.DeserializeObject(responseString);
 
-            var results = new List<SearchResult>();
+            var results = new List<SearchImageResult>();
             foreach (var item in jsonData.items)
             {
-                results.Add(new SearchResult
+                results.Add(new SearchImageResult
                 {
                     Title = item.title,
                     Link = item.link,
-                    Snippet = item.snippet,
                 });
             }
-            return View(results.ToList());
+            return View("ShowImageResults", results.ToList());
         }
     }
 }
